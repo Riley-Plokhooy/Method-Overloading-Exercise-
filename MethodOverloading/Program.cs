1b5a45c23f15368afcd05fb6d5d56ed0722dc1dd
Riley Plokhooy <rileyanthony2006@gmail.com>
2024-11-05 12:37:31 -0500

    done

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,38 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int numOne, int numTwo) 
         {
-            Console.WriteLine("Hello, World!");
+            return numOne + numTwo;
         }
+
+        public static decimal Add(decimal numOne, decimal numTwo) 
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isMoney) 
+        {
+            var sum  = numOne + numTwo;
+
+
+            if (isMoney && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isMoney && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else 
+            {
+                return sum.ToString();
+            }
+        }
+        static void Main(string[] args) 
+        {
+            Console.WriteLine(Add(2, 2, true));
+        }
+        
     }
 }
